c867f8389784f69a09a1f39267a69119564f792b
Julien Priam <julien.priam@gmail.com>
2022-10-05 17:19:16 +0200

    Creation of a Python script

---
+++ Py_files/main.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python2.7
+
+import dpkt
+
+counter = 0
+ipcounter = 0
+tcpcounter = 0
+udpcounter = 0
+
+filename = '/home/eii/Documents/Strathclyde/Strath_Project/Dataset/Thursday-WorkingHours.pcap'
+
+for ts, pkt in dpkt.pcap.Reader(open(filename, 'r')):
+
+    counter += 1
+    eth = dpkt.ethernet.Ethernet(pkt)
+    if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+        continue
+
+    ip = eth.data
+    ipcounter += 1
+
+    if ip.p == dpkt.ip.IP_PROTO_TCP:
+        tcpcounter += 1
+
+    if ip.p == dpkt.ip.IP_PROTO_UDP:
+        udpcounter += 1
+
+print("Total number of packets in the pcap file: ", counter)
+print("Total number of ip packets: ", ipcounter)
+print("Total number of tcp packets: ", tcpcounter)
+print("Total number of udp packets: ", udpcounter)
